d5066f670c4961842235d0e9352bcf3f4bc31762
symphony <xlsymphonylx@gmail.com>
2026-07-25 10:05:22 -0600

    feat: add lazygit toggle in floating terminal
    - Add lazygit via toggleterm as a floating terminal
    - <leader>gg toggles lazygit in/out
    - Restore <leader>g -> Git which-key group

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -1,15 +1,25 @@
 return {
   "akinsho/toggleterm.nvim",
   version = "*",
-  opts = {
-    size = 15,
-    open_mapping = false, -- we handle keybinds manually
-    direction = "horizontal",
-    hide_numbers = true,
-    auto_scroll = true,
-  },
   keys = {
     { "<leader>th", "<cmd>ToggleTerm direction=horizontal<CR>", desc = "Horizontal terminal" },
     { "<leader>tv", "<cmd>ToggleTerm direction=vertical<CR>", desc = "Vertical terminal" },
+    { "<leader>gg", "<cmd>LazyGit<CR>", desc = "Lazygit (float)" },
   },
+  config = function()
+    require("toggleterm").setup({
+      size = 15,
+      open_mapping = false,
+      direction = "horizontal",
+      hide_numbers = true,
+      auto_scroll = true,
+    })
+
+    vim.api.nvim_create_user_command("LazyGit", function()
+      require("toggleterm.terminal").Terminal:new({
+        cmd = "lazygit",
+        direction = "float",
+      }):toggle()
+    end, {})
+  end,
 }
--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -7,6 +7,7 @@ return {
 			mode = { "n", "v" },
 			["<leader>b"] = { name = "Buffer" },
 			["<leader>f"] = { name = "Find" },
+			["<leader>g"] = { name = "Git" },
 			["<leader>h"] = { name = "Hunk" },
 			["<leader>s"] = { name = "Search" },
 			["<leader>t"] = { name = "Terminal" },
